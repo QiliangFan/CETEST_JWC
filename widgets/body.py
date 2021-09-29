640c399cab375d526bb17358a238afb34a58c467
liang <1035847702@qq.com>
2021-09-29 13:51:53 +0800

    'update'

--- widgets/body.py
+++ widgets/body.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QHBoxLayout
 from PyQt5.QtWidgets import QApplication
-from .utils import signed_teachers
-
+from .utils import signed_teachers, main_monitors, candidates, jin_nan, balitai
+import pandas as pd
 
 button_style = """
 QPushButton {
@@ -36,21 +36,27 @@ class Body(QWidget):
         self.main_monitor = QPushButton(self)
         self.main_monitor.setText("担任过主考官的名单")
         self.main_monitor.setStyleSheet(button_style)
+        self.main_monitor.clicked.connect(lambda : main_monitors(self, self.main_monitor))
+
 
         # 候选的老师
         self.candidate = QPushButton(self)
         self.candidate.setText("候选老师名单")
         self.candidate.setStyleSheet(button_style)
+        self.candidate.clicked.connect(lambda : candidates(self, self.candidate))
 
         # 津南考场
         self.jin_nan_exam_room = QPushButton(self)
         self.jin_nan_exam_room.setText("津南考场名单")
         self.jin_nan_exam_room.setStyleSheet(button_style)
+        self.jin_nan_exam_room.clicked.connect(lambda : jin_nan(self, self.jin_nan_exam_room))
+
 
         # 八里台考场
         self.balitai_exam_room = QPushButton(self)
         self.balitai_exam_room.setText("八里台考场名单")
         self.balitai_exam_room.setStyleSheet(button_style)
+        self.balitai_exam_room.clicked.connect(lambda :balitai(self, self.balitai_exam_room))
 
         # 提交
         self.submit = QPushButton(self)
@@ -74,4 +80,35 @@ class Body(QWidget):
         main_vertical.addLayout(room_horizontal)
         main_vertical.addWidget(self.submit)
         self.setLayout(main_vertical)
+
+    # 报名老师名单
+    def update_sign(self, dt: pd.DataFrame):
+        self.sign_list = dt
+
+    # 当过主考官的老师列表
+    def update_mm(self, dt: pd.DataFrame):
+        self.mm_list = dt
+
+    # 候选老师列表
+    def update_candidate(self, dt: pd.DataFrame):
+        self.candidate_list = dt
+
+    # 津南考场名单
+    def update_jinnan(self, dt: pd.DataFrame):
+        self.jin_nan_list = dt
+
+    # 八里台考场名单
+    def update_balitai(self, dt: pd.DataFrame):
+        self.balitai_list = dt
+
+    def get_status(self, campus: str = "津南校区"):
+        assert campus in ['津南校区', '八里台校区']
+        room_in_campus = self.jin_nan_list if campus == "津南校区" else self.balitai_list
+        num_of_mm = len(room_in_campus)
+
+        sign_in_campus = self.sign_list[self.sign_list["校区"]==campus]
+        mm_in_campus = sign_in_campus[sign_in_campus["监考教师姓名"].isin(self.mm_list["姓名"].tolist())]
+        # 在该校区的报名老师但不是当过主监考的老师
+        candidates_in_campus = self.candidate[(self.candidate["姓名"].isin(sign_in_campus["监考教师姓名"].tolist())) & ~(self.candidate["姓名"].isin(mm_in_campus["姓名"].tolist()))]
+
 
--- widgets/utils.py
+++ widgets/utils.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from PyQt5.QtWidgets import QFileDialog, QWidget, QPushButton
 
+from widgets.body import Body
 
-def __read_file(parent):
+
+def __read_file(parent) -> pd.DataFrame:
     file_selector = QFileDialog(parent, filter="Table (*.csv)")
     file_path = file_selector.getOpenFileName()[0]
-    return file_path
+    csv = pd.read_csv(file_path)
+    return csv
 
 def add_right(func):
     def wrapper(p, w: QPushButton):
@@ -20,24 +23,27 @@ def add_right(func):
 
 
 @add_right
-def signed_teachers(parent, widget: QPushButton):
-    csv_file = __read_file(parent)
-
-
-@add_right
-def main_monitors(parent, widget: QWidget):
-    csv_file = __read_file(parent)
-
+def signed_teachers(parent: Body, widget: QPushButton):
+    dt = __read_file(parent)
+    parent.update_sign(dt)
 
 @add_right
-def candidates(parent, widget: QWidget):
-    csv_file = __read_file(parent)
+def main_monitors(parent: Body, widget: QPushButton):
+    dt = __read_file(parent)
+    parent.update_mm(dt)
 
 @add_right
-def jin_nan(parent, widget: QWidget):
-    csv_file = __read_file(parent)
+def candidates(parent: Body, widget: QPushButton):
+    dt = __read_file(parent)
+    parent.update_candidate(dt)
 
 @add_right
-def balitai_nan(parent, widget: QWidget):
-    csv_file = __read_file(parent)
+def jin_nan(parent: Body, widget: QPushButton):
+    dt = __read_file(parent)
+    parent.update_jinnan(dt)
+
+@add_right
+def balitai(parent: Body, widget: QPushButton):
+    dt = __read_file(parent)
+    parent.update_balitai(dt)
 
