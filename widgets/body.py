ef4c4d18e74853c91e07fad2967c6930afb78410
liang <1035847702@qq.com>
2021-09-29 21:44:43 +0800

    hh

--- widgets/body.py
+++ widgets/body.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QHBoxLayout
 from PyQt5.QtWidgets import QApplication
-from .utils import signed_teachers, main_monitors, candidates, exam_room
+from .utils import signed_teachers, main_monitors, candidates, exam_room, submit
 import pandas as pd
 
 button_style = """
@@ -25,8 +25,6 @@ class Body(QWidget):
         self.setGeometry(100, 100, QApplication.desktop().width() // 2, QApplication.desktop().height() // 2)
         self.init()
 
-        self.used = set()
-
     def init(self):
         # 报名的老师
         self.sign_up = QPushButton(self)
@@ -40,24 +38,22 @@ class Body(QWidget):
         self.main_monitor.setStyleSheet(button_style)
         self.main_monitor.clicked.connect(lambda : main_monitors(self, self.main_monitor))
 
-
         # 候选的老师
         self.candidate = QPushButton(self)
         self.candidate.setText("候选老师名单")
         self.candidate.setStyleSheet(button_style)
         self.candidate.clicked.connect(lambda : candidates(self, self.candidate))
-
         # 考场
         self.exam_room = QPushButton(self)
         self.exam_room.setText("考场名单(津南四级|津南六级|八里台四级|八里台六级)")
         self.exam_room.setStyleSheet(button_style)
         self.exam_room.clicked.connect(lambda : exam_room(self, self.exam_room))
 
-
         # 提交
         self.submit = QPushButton(self)
         self.submit.setText("提交")
         self.submit.setStyleSheet(button_style)
+        self.submit.clicked.connect(lambda : submit(self))
 
         # teacher layout
         teacher_horizontal = QHBoxLayout()
@@ -96,27 +92,134 @@ class Body(QWidget):
             balitai4_dt: pd.DataFrame,
             balitai6_dt: pd.DataFrame
     ):
-        self.jin_nan4_dt = jin_nan4_dt
-        self.jin_nan6_dt = jin_nan6_dt
-        self.balitai4_dt = balitai4_dt
-        self.balitai6_dt = balitai6_dt
+        self.jin_nan4_dt = jin_nan4_dt if len(jin_nan4_dt) > 0 and "发卷点" in jin_nan4_dt.columns else None
+        self.jin_nan6_dt = jin_nan6_dt if len(jin_nan6_dt) > 0 and "发卷点" in jin_nan6_dt.columns else None
+        self.balitai4_dt = balitai4_dt if len(balitai4_dt) > 0 and "发卷点" in balitai4_dt.columns else None
+        self.balitai6_dt = balitai6_dt if len(balitai6_dt) > 0 and "发卷点" in balitai6_dt.columns else None
 
     def get_status(self, campus: str = "津南校区", exam_type: str = "4"):
+        print(f"===> {campus} ==== {exam_type}")
         assert campus in ['津南校区', '八里台校区']
-        room_in_campus = self.jin_nan_list if campus == "津南校区" else self.balitai_list
+        assert exam_type in ["4", "6"]
+        used = set()
+
+        if exam_type == "4":
+            room_in_campus = self.jin_nan4_dt if campus == "津南校区" else self.balitai4_dt
+        else:
+            room_in_campus = self.jin_nan6_dt if campus == '津南校区' else self.balitai6_dt
         num_of_mm_needed = len(room_in_campus)
 
         # 对应校区、对应考试等级的报名老师
-        sign_in_campus = self.sign_list[(self.sign_list["校区"]==campus) & (self.sign_list["级别"].str.contains(exam_type)) & (~self.sign_list["监考教师姓名"].isin(self.used))]
+        sign_in_campus = self.sign_list[(self.sign_list["校区"]==campus) & (self.sign_list["级别"].str.contains(exam_type))]
         mm_in_campus = sign_in_campus[sign_in_campus["监考教师姓名"].isin(self.mm_list["姓名"].tolist())]
         # 在该校区的报名老师但不是当过主监考的老师
-        candidates_in_campus = self.candidate[(self.candidate["姓名"].isin(sign_in_campus["监考教师姓名"].tolist())) & ~(self.candidate["姓名"].isin(mm_in_campus["姓名"].tolist()))]
+        candidates_in_campus = self.candidate_list[
+            (self.candidate_list["姓名"].isin(sign_in_campus["监考教师姓名"].tolist())) & \
+            ~(self.candidate_list["姓名"].isin(mm_in_campus["监考教师姓名"].tolist()))
+        ]
 
-        # 担任过主监考官的教师人数大于教室数目
-        if len(mm_in_campus) >= num_of_mm_needed:
-            pass
-        else:
-            pass
+        # 男 / 女 -> 单位
+        mm_dict = {}
+        not_mm_dict = {}
 
-    def reset(self):
-        self.used.clear()
+        # 不同单位尽量不要在一起
+        num = 0
+        for (gender, danwei), group in mm_in_campus.groupby(["性别", "所在单位"], dropna=False):
+            if str(gender) not in mm_dict:
+                mm_dict[gender] = {}
+            if danwei not in mm_dict[gender]:
+                mm_dict[gender][danwei] = set()
+            num_left = num_of_mm_needed - num
+            teacher_set = set(group["监考教师姓名"].tolist()[:num_left])
+            mm_dict[gender][danwei].update(teacher_set)
+            used.update(teacher_set)
+            num += len(teacher_set)
+
+        # 从候选里面选择
+        if num < num_of_mm_needed:
+            candidate = sign_in_campus[sign_in_campus["监考教师姓名"].isin(candidates_in_campus["姓名"].tolist())]
+            for (gender, danwei), group in candidate.groupby(["性别", "所在单位"], dropna=False):
+                if str(gender) not in mm_dict:
+                    mm_dict[gender] = {}
+                if danwei not in mm_dict[gender]:
+                    mm_dict[gender][danwei] = set()
+                num_left = num_of_mm_needed - num
+                teacher_set = set(group["监考教师姓名"].tolist()[:num_left])
+                mm_dict[gender][danwei].update(teacher_set)
+                used.update(teacher_set)
+                num += len(teacher_set)
+                if num >= num_of_mm_needed:
+                    break
+
+        # 从参加过的老师里面选
+        if num < num_of_mm_needed:
+            exped = sign_in_campus[(~sign_in_campus["监考教师姓名"].isin(used)) & (sign_in_campus["是否参加过"] == "参加过")]
+            for (gender, danwei), group in exped.groupby(["性别", "所在单位"], dropna=False):
+                if str(gender) not in mm_dict:
+                    mm_dict[gender] = {}
+                if danwei not in mm_dict[gender]:
+                    mm_dict[gender][danwei] = set()
+                num_left = num_of_mm_needed - num
+                teacher_set = set(group["监考教师姓名"].tolist()[:num_left])
+                mm_dict[gender][danwei].update(teacher_set)
+                used.update(teacher_set)
+                num += len(teacher_set)
+                if num >= num_of_mm_needed:
+                    break
+        print(f"mm_len: {num}")
+
+        # 从所有未安排为主考官的老师里面选择
+        not_mm = sign_in_campus[
+            ~(sign_in_campus["监考教师姓名"].isin(self.mm_list["姓名"].tolist())) & \
+            ~(sign_in_campus["监考教师姓名"].isin(candidates_in_campus["姓名"].tolist())) & \
+            ~(sign_in_campus["监考教师姓名"].isin(used))
+        ]
+        not_num = 0
+
+        for (gender, danwei), group in not_mm.groupby(["性别", "所在单位"], dropna=False):
+            if gender not in not_mm_dict:
+                not_mm_dict[gender] = {}
+            if danwei not in not_mm_dict[gender]:
+                not_mm_dict[gender][danwei] = set()
+            teacher_set = set(group["监考教师姓名"].tolist())
+            not_mm_dict[gender][danwei].update(teacher_set)
+            not_num += len(teacher_set)
+        print(f"not_mm_len: {not_num} |  total: {len(not_mm)}")
+
+        result = set()
+        used.clear()
+        for gender in ["男", "女"]:
+            for campus_tmp in mm_dict[gender]:
+                for teacher in mm_dict[gender][campus_tmp]:
+                    if teacher not in used:
+                        _pair = None
+                        for _gender in not_mm_dict:
+                            if _pair is not None: break
+                            if _gender != gender:
+                                for _campus in not_mm_dict[_gender]:
+                                    if _pair is not None: break
+                                    if _campus != campus_tmp:
+                                        for _teacher in not_mm_dict[_gender][_campus]:
+                                            if _teacher not in used:
+                                                _pair = _teacher
+                                                break
+                        if _pair is None:  # 如果不能男女搭配, 就女女搭配
+                            for _campus in not_mm_dict["女"]:
+                                if _pair is not None: break
+                                if _campus != campus_tmp:
+                                    for _teacher in not_mm_dict["女"][_campus]:
+                                        if _teacher not in used:
+                                            _pair = _teacher
+                                            break
+                        if _pair is not None:
+                            result.add((teacher, _pair))
+                            used.add(teacher)
+                            used.add(_pair)
+                            _pair = None
+                        else:
+                            print("未匹配教师: ", teacher, gender)
+        assert len(result) == len(room_in_campus), f"分配配对数{len(result)}, 所需的配对数{len(room_in_campus)}"
+        assign = {}
+        for room, (t1, t2) in zip(room_in_campus["发卷点"].tolist(), result):
+            assign[str(room)] = [t1, t2]
+        print(f"{campus} - {exam_type} - Finish!")
--- widgets/utils.py
+++ widgets/utils.py
@@ -1,17 +1,22 @@
 import pandas as pd
 from PyQt5.QtWidgets import QFileDialog, QWidget, QPushButton
-from typing import Union, List
+from typing import Union, List, cast
 
 
-def __read_file(parent, sheet_name: Union[int, List] = 0) -> Union[pd.DataFrame, dict]:
-    file_selector = QFileDialog(parent, filter="Table (*.csv)")
+def __read_file(parent, sheet_name: Union[int, List] = 0) -> Union[pd.DataFrame, dict, None]:
+    file_selector = QFileDialog(parent, filter="Table (*.csv, *.xlsx, *.xls)")
     file_path = file_selector.getOpenFileName()[0]
-    csv = pd.read_excel(file_path, sheet_name)
+    if len(file_path.strip()) == 0:
+        return None
+    if file_path.endswith(".xlsx") or file_path.endswith(".xls"):
+        csv = pd.read_excel(file_path, sheet_name)
+    else:
+        csv = pd.read_csv(file_path)
     if isinstance(csv, pd.DataFrame):
-        csv = csv.applymap(lambda x: str(x).strip() if isinstance(x, str) else x, na_action="ignore")
+        csv = csv.applymap(lambda x: str(x).strip() if isinstance(x, str) else str(x), na_action="ignore")
     elif isinstance(csv, dict):
         for key in csv:
-            csv[key] = csv[key].applymap(lambda x: str(x).strip() if isinstance(x, str) else x, na_action="ignore")
+            csv[key] = csv[key].applymap(lambda x: str(x).strip() if isinstance(x, str) else str(x).strip(), na_action="ignore")
     else:
         raise ValueError()
     return csv
@@ -31,17 +36,20 @@ def add_right(func):
 @add_right
 def signed_teachers(parent, widget: QPushButton):
     dt = __read_file(parent)
-    parent.update_sign(dt)
+    if dt is not None:
+        parent.update_sign(dt)
 
 @add_right
 def main_monitors(parent, widget: QPushButton):
     dt = __read_file(parent)
-    parent.update_mm(dt)
+    if dt is not None:
+        parent.update_mm(dt)
 
 @add_right
 def candidates(parent, widget: QPushButton):
     dt = __read_file(parent)
-    parent.update_candidate(dt)
+    if dt is not None:
+        parent.update_candidate(dt)
 
 @add_right
 def exam_room(parent, widget: QPushButton):
@@ -49,12 +57,45 @@ def exam_room(parent, widget: QPushButton):
         parent,
         sheet_name=["津南四级", "津南六级", "八里台四级", "八里台六级"]
     )
-    parent.update_exam_room(
-        dt["津南四级"],
-        dt["津南六级"],
-        dt["八里台四级"],
-        dt["八里台六级"]
-    )
+    if dt is not None:
+        parent.update_exam_room(
+            dt["津南四级"],
+            dt["津南六级"],
+            dt["八里台四级"],
+            dt["八里台六级"]
+        )
 
 
+def submit(parent):
+    from .body import Body
+    parent = cast(Body, parent)
+    result = {}
+    if parent.jin_nan4_dt is not None:
+        try:
+            result["jinnan4"] = parent.get_status(campus="津南校区", exam_type="4")
+        except:
+            import traceback
+            traceback.print_exc()
+    if parent.jin_nan6_dt is not None:
+        try:
+            result["jinnan6"] = parent.get_status(campus="津南校区", exam_type="6")
+        except:
+            import traceback
+            traceback.print_exc()
 
+    if parent.balitai4_dt is not None:
+        try:
+            result["balitai4"] = parent.get_status(campus="八里台校区", exam_type="4")
+        except:
+            import traceback
+            traceback.print_exc()
+
+
+    if parent.balitai6_dt is not None:
+        try:
+            result["balitai6"] = parent.get_status(campus="八里台校区", exam_type="6")
+        except:
+            import traceback
+            traceback.print_exc()
+
+    print("debug")
